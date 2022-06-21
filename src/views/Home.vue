301eac210de83fa4317fa8399f20e1e6e6e63a78
JulianaBMattos <julianabarcellosmattos@gmail.com>
2022-06-21 19:01:54 +0100

    general structure

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <section class="box">
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <!-- <div class="title">{{$t('general.TITLE')}}</div> -->
+          <div class="title">TITLE</div>
+          <!-- <div class="subtitle" v-html="$t('general.SUBTITLE')"></div> -->
+          <div class="subtitle">SUB-TITLE</div>
+        </div>
+        <div class="column is-12">
+          <!-- <div class="content" v-html="$t('general.EXPLANATION')"></div> -->
+          <div class="content">GENERAL_EXPLANATION</div>
+        </div>
+      </div>
+      <div class="buttons is-flex is-justify-content-center">
+        <b-button tag="router-link"
+          :to="{ name: 'about' }" type="is-primary is-large">
+          <!-- {{ $t('general.START') }} -->
+          {{ 'ABOUT' }}
+        </b-button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
