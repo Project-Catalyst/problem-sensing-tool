111b8abe82c01122a58cd5ec18e3f0a01b01dba2
JulianaBMattos <julianabarcellosmattos@gmail.com>
2022-07-07 22:57:59 +0100

    added publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+let publicPath = ''
+if (process.env.APP_ENV === 'production') {
+  publicPath = '/problem-sensing-tool/'
+}
+if (process.env.APP_ENV === 'staging') {
+  publicPath = '/'
+}
+if (process.env.APP_ENV === 'development') {
+  publicPath = '/'
+}
+module.exports = {
+  publicPath: publicPath
+}
